b279de890d39fba5476a0b59364aa0a9b2b04d0e
user <user@10.32.110.122>
2020-10-15 18:27:01 +0100

    Working for some values

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,46 +1,82 @@
 
 import java.util.ArrayList; 
 import java.util.List; 
-  
-//Creating nodes for tree here
-class Node 
+ 
+public class LCA
 {
-    int value;
-    Node left;
-    Node right;
- 
-    Node(int value)
-    {
-        this.value = value;
-        right = null;
-        left = null;
-    }
-
-	//Creating tree with each node as had issues when comparing binarytree type to nodes
-	public static Node createTree()
-	{
- 
-		Node rootNode = new Node(8);		//	    8
-		Node node5 = new Node(5);			//	   / \
-		Node node10 = new Node(10);			//    5   10 	 
-		Node node2 = new Node(2);			//   / \  / \
-		Node node6 = new Node(6);			//  2   6 3  12
-		Node node3 = new Node(3);
-		Node node12 = new Node(12);
- 
-		rootNode.left = node5;
-		rootNode.right = node10;
- 
-		node5.left = node2;
-		node2.right = node6;
- 
-		node10.left = node3;
-		node10.right = node12;
-
-		return rootNode;
-	}
-	 
-	 
-
+		//Creating nodes for tree here
+		public static class Node 
+		{
+		    int value;
+		    Node left;
+		    Node right;
+		 
+		    Node(int value)
+		    {
+		        this.value = value;
+		    }
+		
+			//Creating tree with each node as had issues when comparing binarytree type to nodes
+			public static Node createTree()
+			{
+		 
+				Node rootNode = new Node(8);		//	    8
+				Node node5 = new Node(5);			//	   / \
+				Node node10 = new Node(10);			//    5   10 	 
+				Node node2 = new Node(2);			//   / \  / \
+				Node node6 = new Node(6);			//  2   6 3  12
+				Node node3 = new Node(3);
+				Node node12 = new Node(12);
+		 
+				rootNode.left = node5;
+				rootNode.right = node10;
+		 
+				node5.left = node2;
+				node2.right = node6;
+		 
+				node10.left = node3;
+				node10.right = node12;
+		
+				return rootNode;
+			}
+		
+			public static Node LCA(Node root, Node a, Node b) {
+				
+				if(root == null)
+				{
+					return null;
+				}
+				
+				if(root.value == a.value || root.value == b.value )
+				{
+					return root;
+				}
+				
+				Node left = LCA(root.left,a,b); //checks if lca is next node on left
+				Node right=LCA(root.right,a,b); //checks if lca is next node on right
+				
+				if(left!=null && right!=null)
+				{
+					return root;
+				}
+				
+				if(left== null)
+				{
+					return right;
+				}
+				else
+				{
+					return left;
+				}
+			}
+		 
+		 public static void main(String[] args) {
+				// TODO Auto-generated method stub
+			 	Node rootNode = createTree();
+				Node node3 = new Node(3);
+				Node node10 = new Node(10);
+				System.out.println("Lowest common ancestor for node 3 and 10:" + LCA(rootNode,node3,node10).value);
+			 }
+		}
 
 }
