b8a35f739dc3407aa0bda7fb73fccff4d493af97
user <user@10.32.110.122>
2020-10-23 01:27:22 +0100

    Python code finished and 1 test done and works from finding LCA. Second repositary setup did not realise we needed them in seperate ones so only last commits going to this repositary for python. Rest of commits in LCA

---
+++ LCAPython/src/TestLCA.py
@@ -0,0 +1,28 @@
+'''
+Created on 22 Oct 2020
+
+@author: user
+'''
+import unittest
+import LCA
+from LCA import findLCA
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+     
+    def testLca(self): 
+
+        root = LCA.Node(1) 
+        root.left = LCA.Node(2) 
+        root.right = LCA.Node(3) 
+        root.left.left = LCA.Node(4) 
+        root.left.right = LCA.Node(5) 
+        root.right.left = LCA.Node(6) 
+        root.right.right = LCA.Node(7)
+        self.assertEquals(2,findLCA(root,4,5))
+        
+        
